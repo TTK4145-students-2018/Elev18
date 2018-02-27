5265e137568d1a92779229056668d02253138f94
Edvard Narum <efnarum@stud.ntnu.no>
2018-02-27 15:34:24 +0100

    add tolerance of unexpected events in fsm

--- fsm.erl
+++ fsm.erl
@@ -8,7 +8,10 @@ st_init() ->
 	io:format("fsm: initializing"),
 	receive ev_ground_floor_reached ->
 		io:format("fsm: elevator initialized, behold my initial glory"),
-		st_idle()
+		st_idle();
+		Other ->
+			io:format("fsm_init: received garbage:" ~p~n, [Other]),
+			st_init()
 
 	after 10000 ->
 		io:format("fsm: init timed out, trying again");
@@ -23,8 +26,11 @@ st_idle() ->
 			st_moving();
 
 		ev_emergency_stop ->
-			st_emergency()
+			st_emergency();
 
+		Other ->
+			io:format("fsm_idle: received garbage:" ~p~n, [Other]),
+			st_idle()
 	end.
 
 
@@ -36,7 +42,11 @@ st_moving() ->
 			st_doors_open();
 
 		ev_emergency_stop ->
-			st_emergency()
+			st_emergency();
+
+		Other ->
+			io:format("fsm_moving: received garbage:" ~p~n, [Other]),
+			st_moving()
 
 	end.
 
@@ -44,7 +54,10 @@ st_doors_open() ->
 	io:format("doors opened"),
 	receive
 		order_recieved ->
-			st_moving()
+			st_moving();
+
+		Other ->
+			io:format("fsm_doors_open: received garbage:" ~p~n, [Other]),
 
 	after 2000 ->
 		st_idle()
@@ -54,6 +67,13 @@ st_emergency() ->
 	io:format("fsm: emergency state activated"),
 	receive
 		order_recieved ->
-			st_moving()
+			st_moving();
+
+		ev_emergency_stop ->
+			st_emergency();
+
+		Other ->
+			io:format("fsm_emergency: received garbage:" ~p~n, [Other]),
+			st_emergency()
 
 	end.
