aabf798ca85860a03463c2006612c37d6edfdb02
Edvard Narum <efnarum@stud.ntnu.no>
2018-02-27 14:23:43 +0100

    fix syntax

--- fsm.erl
+++ fsm.erl
@@ -5,17 +5,17 @@ start() ->
 	st_init().
 
 st_init() ->
-	io:format("fsm: initializing").
+	io:format("fsm: initializing"),
 	fsm_handler ! {initializing},
 	receive ev_ground_floor_reached ->
 		fsm_handler ! {init_complete}
-		io:format("fsm: elevator initialized, behold my initial glory").
+		io:format("fsm: elevator initialized, behold my initial glory"),
 	end,
 
 	st_idle().
 
 st_idle() ->
-	io:format("fsm: elevator idle").
+	io:format("fsm: elevator idle"),
 	fsm_handler ! {idle},
 	receive 
 		order_recieved ->
@@ -41,22 +41,22 @@ st_moving() ->
 	end.
 
 st_doors_open() ->
-	io:format("doors opened").
-	fsm_handler ! {doors_open}.
+	io:format("doors opened"),
+	fsm_handler ! {doors_open},
 	receive
 		order_recieved ->
 			st_moving()
 
 	after 2000 ->
-		fsm_handler ! {doors_close}.
-		st_idle().
+		fsm_handler ! {doors_close},
+		st_idle()
 	end.
 
 st_emergency() ->
-	io:format("fsm: emergency state activated").
-	fsm_handler ! {emergency}.
+	io:format("fsm: emergency state activated"),
+	fsm_handler ! {emergency},
 	receive
 		order_recieved ->
-			st_moving();
+			st_moving()
 
 	end.
