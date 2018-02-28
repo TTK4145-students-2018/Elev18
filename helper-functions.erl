d25c592942c1a1d1477187b4d384ff1c272c6603
Edvard Narum <efnarum@stud.ntnu.no>
2018-02-28 13:21:45 +0100

    add helper functions factorial and length of list

---
+++ helper-functions.erl
@@ -0,0 +1,13 @@
+-module(hf).
+-export([len/1, fac/2]).
+
+len([]) -> 0;
+len([_|L]) -> 1 + len(L). %returns length of a list
+
+fac(0,A) when A /= 0 -> 0; %0^A = 0
+fac(N,0) -> 1; %N^0 = 1
+fac(N,A) when A > 0 -> N*fac(N,A-1); %returns N^A
+fac(N,A) when A < 0 -> 1/fac(N,(-1)*A);
+
+
+
