0f72d110fac40fcd966ab527dcda5bccd2ad5156
Edvard Narum <efnarum@stud.ntnu.no>
2018-02-28 14:44:57 +0100

    change name to hf, fix garbage

--- hf.erl
+++ hf.erl
@@ -1,13 +1,16 @@
 -module(hf).
--export([len/1, fac/2]).
+-export([len/1, fac/2, flip/1]).
 
 len([]) -> 0;
 len([_|L]) -> 1 + len(L). %returns length of a list
 
+
 fac(0,A) when A /= 0 -> 0; %0^A = 0
 fac(N,0) -> 1; %N^0 = 1
 fac(N,A) when A > 0 -> N*fac(N,A-1); %returns N^A
-fac(N,A) when A < 0 -> 1/fac(N,(-1)*A);
+fac(N,A) when A < 0 -> 1/fac(N,(-1)*A).
 
 
+flip([]) -> [];
+flip([H|T]) -> [lists:last([H|T])] ++ flip(lists:droplast([H|T])).
 
