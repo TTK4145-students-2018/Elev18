fee3111eab228ee489ff089ae3e694a937e8dfea
Edvard Narum <efnarum@stud.ntnu.no>
2018-03-16 15:37:39 +0100

    update description

--- scheduler.erl
+++ scheduler.erl
@@ -1,8 +1,14 @@
 -module(scheduler).
 -export([]).
 
-% scheduler is the module calculating cost
-% and returning whether or not the node it runs
-% on is to accept the task it has been fed
+% Calculates cost, and determines whether it is optimal 
+% for this elevator to perform a task instead of one of the
+% others. Keeps track of worldview* and own state (from fsm) to
+% serve as a basis of calculations. Needs the same worldview as
+% the other schedulers, to make sure every scheduler reaches
+% decisions based on correct information about the entire system. 
 
 % TODO:
+% 
+
+evaluate(PID, )
