721276bfd8b872542c13fa36c0ddce4f9220fa5d
Johan Brun <johannicbrun@gmail.com>
2018-04-04 14:10:14 +0200

    net_init generates unique nodename using IP adress of computer

--- network.erl
+++ network.erl
@@ -1,17 +1,26 @@
 -module(network).
--export(start/0).
+-export([net_init/0]).
 
-%Network module uses distibuted Erlang (node cluster)
-%to communicate with other nodes
+% Network module uses distibuted Erlang (node cluster)
+% to communicate with other nodes
 
 
-%TO DO
-%Establish connection with other nodes
-%Distribute worldview
-%Distribute orders
+% TO DO
+% Establish connection with other nodes
+% Distribute worldview
+% Distribute orders
 
-start ->
-	net_init().
+net_init() ->
+	os:cmd("epmd -daemon"),
 
-net_init ->
-	
+	{ok, IPlist} = inet:getif(), 							% inet:getif() gives a list of tuples of IPs
+	IP = element(1, hd(IPlist)), 							% Header of IPlist is local IP adress
+	NodeName = list_to_atom("heis@" ++ format_IP(IP)). 		% 
+
+ 	net_kernel:start()
+
+
+% Formats IP from a tuple to a string
+format_IP(IPtuple) ->
+	[_Head | IPlist] = lists:flatmap(fun(X) -> ['.', X] end, tuple_to_list(IPtuple)),
+	lists:concat(IPlist).
