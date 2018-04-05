a03d00b429d05d947ff8e9cd8e8b62901d00d4bd
Thomas Sundvoll <thomas@sundvoll.net>
2018-04-05 14:08:43 +0200

    message format

--- network.erl
+++ network.erl
@@ -1,11 +1,14 @@
 -module(network).
 -export([net_init/0]).
 
+
+
 % Network module uses distibuted Erlang (node cluster)
 % to communicate with other nodes
 
 
 % TO DO
+% 
 % Establish connection with other nodes
 % Distribute worldview
 % Distribute orders
@@ -13,15 +16,17 @@
 net_init() ->
 	os:cmd("epmd -daemon"),
 
-	{ok, IPlist} = inet:getif(), 							% inet:getif() gives a list of tuples of IPs
-	IP = element(1, hd(IPlist)), 							% Header of IPlist is local IP adress
-	NodeName = list_to_atom("elevator@" ++ format_IP(IP)), 	% generates a unique nodename
+	{ok, LongIPlist} = inet:getif(), 							% inet:getif() gives a list of tuples of IPs
+	IPlist = tuple_to_list(element(1, hd(LongIPlist))), 							% Header of IPlist is local IP adress 
+
+	NodeName = list_to_atom("elevator@" ++ format_IP(IPtuple)), 	% generates a unique nodename
+
+    %[IP|_T] = hf:flip(IP),
 
  	net_kernel:start([NodeName, longnames, 500]),			% Creates node with heartbeat of 500 milliseconds 
  	erlang:set_cookie(node(), 'glue'),
 
 	Hosts = net_adm:host_file(),
-	Hosts.
 
 
 
@@ -29,6 +34,6 @@ net_init() ->
 
 
 % Formats IP from a tuple to a string - from Sivertba's ErlangHeis
-format_IP(IPtuple) ->
-	[_Head | IPlist] = lists:flatmap(fun(X) -> ['.', X] end, tuple_to_list(IPtuple)),
-	lists:concat(IPlist).
+format_IP(IPlist) ->
+	[_Head | IP] = lists:flatmap(fun(X) -> ['.', X] end, IPlist),
+	lists:concat(IP).
