ddf70feb11af95b44f3758fe6d4d6d0e95a292ac
Johan Brun <johannicbrun@gmail.com>
2018-04-11 13:36:56 +0200

    netork getting better

--- network_UDP.erl
+++ network_UDP.erl
@@ -20,14 +20,39 @@ init() ->
     %worldview ! {id, ID},
     
  	net_kernel:start([NodeName, longnames, 500]),				% Creates node with heartbeat of 500 milliseconds 
- 	erlang:set_cookie(node(), 'Elev18').
+ 	erlang:set_cookie(node(), 'Elev18'),
+
+ 	node_center ! {network, init_complete}.
+
+listener() ->
+ 	{ok, ReceiveSocket} = gen_udp:open(?RECV_PORT, [list, {active, false}]),
+ 	listener(ReceiveSocket).
+
+listener(ReceiveSocket) ->
+	{ok, {_Adress, ?SEND_PORT, NodeName}} = gen_udp:recv(RecvSocket, 0),
+	Node = list_to_atom(NodeName),
+	case lists:member(Node, [node()|nodes()]) of
+		true ->
+			listen(ReceiveSocket);
+		false ->
+			net_adm:ping(Node), % ping node to create a connection
+			io:format("Node connected: ~p~n", [Node]), %debug
+			listen(ReceiveSocket)
+		end.
 
 
- %listener() ->
-% 	{ok, ReceiveSocket} = gen_udp:open(?RECV_PORT, [list, {active, false}]),
-% 	listener(ReceiveSocket).
+
 
 % Formats IP from a tuple to a string.
 format_IP(IPlist) ->
 	[_Head | IP] = lists:flatmap(fun(X) -> ['.', X] end, IPlist),
 	lists:concat(IP).
+
+broadcast() ->
+	{ok, SendSocket} = gen_udp:open(?SEND_PORT, [list, {active, true}, {broadcast, true}]),
+	broadcast(SendSocket).
+
+broadcast(SendSocket) ->
+	ok = gen_udp:send(SendSocket, {255,255,255,255}, ?RECEIVE_PORT, atom_to_list(node())),
+	timer:sleep(2000),
+	broadcast(SendSocket).
