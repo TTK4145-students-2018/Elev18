5350f97f52f59e79a93ebb211c3367d216ba206f
Johan Brun <johannicbrun@gmail.com>
2018-04-11 12:55:32 +0200

    Tiny update to sequence

--- node_center.erl
+++ node_center.erl
@@ -7,13 +7,18 @@
 %TODO
 
 start() ->
-	register(fsm, spawn(fun fsm:start/0)),
-	register(order_manager, spawn(fun order_manager:start/0)),
+	register(node_center, self()),
 	{ok, DriverPid} = driver:start(),
 	register(driver, DriverPid),
+	register(network, spawn(fun network:start/0)),
+	receive {network, init_complete} -> ok end,
+
+
 	register(event_manager, spawn(fun event_manager:start/0)),
 	register(worldview, spawn(fun worldview:start/0)),
-	register(network, spawn(fun network:start/0)).
+
+	register(order_manager, spawn(fun order_manager:start/0)),
+	register(fsm, spawn(fun fsm:start/0)).
 	
 
 
