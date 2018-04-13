9a124ebd32e2c578c64ce9a7adccee750be099e1
Johan Brun <johannicbrun@gmail.com>
2018-04-13 17:16:39 +0200

    Scheduler commented out from update_worldviews

--- network.erl
+++ network.erl
@@ -78,17 +78,17 @@ update_worldviews(WorldViews) ->
 			UpdatedViews = reevaluate(DeadOrders, NewViews, OwnID),
 			update_worldviews(UpdatedViews);
 		{order, Order} ->
-			worldview ! {request, wv, update_worldviews},
-			receive {response, wv, WorldView} -> ok end,
-			OwnID = element(1, WorldView),
-			BestID = scheduler:scheduler(WorldViews, Order),
-			case OwnID == BestID of
-				true ->
-					order_manager ! {add, Order},
-					update_worldviews(WorldViews);
-				false ->
-					update_worldviews(WorldViews)
-			end
+			%worldview ! {request, wv, update_worldviews},
+			%receive {response, wv, WorldView} -> ok end,
+			%OwnID = element(1, WorldView),
+			%BestID = scheduler:scheduler(WorldViews, Order),
+			%case OwnID == BestID of
+			%	true ->
+			order_manager ! {add, Order},
+			update_worldviews(WorldViews)
+			%	false ->
+			%		update_worldviews(WorldViews)
+			%end
 	end.
 
 order_distributor() ->
